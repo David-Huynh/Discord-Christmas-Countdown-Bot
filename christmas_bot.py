9f8f9fcaafc064aa62f570b77fd1627c3266c371
david-huynh <huynh.david.work@gmail.com>
2021-06-07 12:54:40 -0400

    added task start

--- christmas_bot.py
+++ christmas_bot.py
@@ -1,15 +1,15 @@
-import datetime
+from datetime import datetime, timedelta
 import discord
+from discord.ext import tasks
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
+    update_name.start()
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    
     if message.author == client.user:
         return
     else:
@@ -18,11 +18,12 @@ async def on_message(message):
 
 @tasks.loop(hours=24)
 async def update_name():
-    date = date.today()
-    christmas_day = datetime.datetime(date.year,12,25)
+    print(client.user.display_name)
+    date = datetime.now()
+    christmas_day = datetime(date.year,12,25)
     delta = christmas_day - date
-    time_change = datetime.timedelta(delta)
-    client.user.edit(username = "IT'S CHRISTMAS IN {} DAYS".format(time_change.days))
+    await client.user.edit(username = "IT'S CHRISTMAS IN {} DAYS".format(delta.days))
+
 
 file = open("token.txt")
 token = file.read()
