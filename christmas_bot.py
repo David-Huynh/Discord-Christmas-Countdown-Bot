e9d2b015ce86e6ba6a89d361a64dee689f08c9da
david-huynh <huynh.david.work@gmail.com>
2021-06-07 13:28:11 -0400

    added msg functionality for every guild every day

--- christmas_bot.py
+++ christmas_bot.py
@@ -5,23 +5,20 @@ from discord.ext import tasks
 client = discord.Client()
 @client.event
 async def on_ready():
-    update_name.start()
+    update_name_send_message.start()
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    else:
-        await message.channel.send('STFU!')
-        await message.channel.send(client.user.display_name)
-
 @tasks.loop(hours=24)
-async def update_name():
+async def update_name_send_message():
     print(client.user.display_name)
     date = datetime.now()
     christmas_day = datetime(date.year,12,25)
     delta = christmas_day - date
+    
+    for guild in client.guilds:
+        print(guild.text_channels)
+        channel = client.get_channel(guild.text_channels[0].id)
+        await channel.send("IT'S CHRISTMAS IN {} DAYS".format(delta.days))
     await client.user.edit(username = "IT'S CHRISTMAS IN {} DAYS".format(delta.days))
 
 
