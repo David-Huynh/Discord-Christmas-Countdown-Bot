1c53b1e5b423885feb3be9770668a41b614dee8f
david-huynh <huynh.david.work@gmail.com>
2021-06-07 12:19:17 -0400

    added update name task

--- christmas_bot.py
+++ christmas_bot.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 
 client = discord.Client()
@@ -8,12 +9,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    test = "200"
-    client.user.edit(username = "IT'S CHRISTMAS IN {} DAYS".format(test))
+    
     if message.author == client.user:
         return
     else:
         await message.channel.send('STFU!')
+        await message.channel.send(client.user.display_name)
+
+@tasks.loop(hours=24)
+async def update_name():
+    date = date.today()
+    christmas_day = datetime.datetime(date.year,12,25)
+    delta = christmas_day - date
+    time_change = datetime.timedelta(delta)
+    client.user.edit(username = "IT'S CHRISTMAS IN {} DAYS".format(time_change.days))
+
 file = open("token.txt")
 token = file.read()
 client.run(token)
